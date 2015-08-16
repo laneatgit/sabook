65eeb0fd72ee91aeaacc11107150c56d88cbbafa
laneatgit <laneatgit@gmail.com>
2015-08-16 22:04:39 +0900

    update 20150816

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+    def trading_title_by_direction(subject_type, opposite_trading_direction)
+
+        if subject_type == '流動資産'
+            return opposite_trading_direction=='借' ? '出金' : '入金'
+        elsif subject_type == '支出'
+            return opposite_trading_direction=='借' ? '割引' : '費用'
+        elsif  subject_type == '収入'
+            return opposite_trading_direction=='借' ? '収入' : '割引'
+        else
+            raise 'wrong in trading_title_by_direction'
+        end    
+
+    end
+
 end
